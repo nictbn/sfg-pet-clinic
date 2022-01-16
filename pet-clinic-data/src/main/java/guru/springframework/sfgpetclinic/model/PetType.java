99b7497309c550c9a39c68c5b2449c6557ff4eec
Nicolae Taban <nicolae.taban@gmail.com>
2022-01-16 19:53:26 +0200

    Fixes parse issue on pet type. Closes #69

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -6,11 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
